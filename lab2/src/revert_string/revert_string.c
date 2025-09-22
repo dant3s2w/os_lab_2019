37beb2eee3c95b9f34d3e2b88f63d271eafa141b
Daniil Nikolaev <nikolaevd337@gmail.com>
2025-09-22 08:17:24 +0300

    добавлен код в revert_string.c и swap.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,14 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int length;
+	while (str[length] != '\0'){
+		length++;
+	}
+	for (int i = 0; i < length/2; i++){
+		char temp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = temp;
+	}
 }
 
